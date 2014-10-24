ca1ae27bea230d1b6eee9b1b5dbe3d8444102331
Chris Bubernak <chris.bubernak@gmail.com>
2014-10-24 09:32:46 -0700

    First Commit

---
+++ platforms/wp8/www/js/game.ts
@@ -0,0 +1,38 @@
+﻿/// <reference path="fly.ts"/>
+
+class Game {
+    static maxLeft: number = window.innerWidth;
+    static maxTop: number = window.innerHeight;
+    static scoreCounter: number = 0;
+    private static fps: number = 20;
+    private static flies: Fly[] = [];
+    private static numOfFlies: number = 20;
+
+    public static score() {
+        Game.scoreCounter++;
+        Game.updateScore();
+    }
+
+    private static updateScore() {
+        document.getElementById("scoreCounter").innerHTML = Game.scoreCounter.toString();
+    }
+
+    public static start() {
+        for (var f = 0; f < Game.numOfFlies; f++) {
+            Game.flies.push(new Fly());
+        }
+
+        // Start the game loop
+        var intervalId = setInterval(Game.run, 1000 / Game.fps);
+        // To stop the game, use the following:
+        //clearInterval(Game._intervalId);
+    }
+
+    private static run () {
+        for (var f = 0; f < Game.flies.length; f++) {
+            Game.flies[f].move();
+        }
+
+        Game.updateScore();
+    }
+}
---
+++ www/js/fly.ts
@@ -0,0 +1,72 @@
+﻿/// <reference path="game.ts"/>
+
+class Fly {
+    width: number = Game.maxLeft/10;
+    height: number = this.width;
+    totalHealth: number = 4;
+    healthRemaining: number;
+    div: HTMLDivElement;
+    moveSpeed: number = Math.round(Math.random() * 7 + 3);
+    angle: number;
+
+    constructor() {
+        var maxLeft = Game.maxLeft - this.width;
+        var maxTop = Game.maxTop - this.height;
+        var x = Math.min(Math.max(0, ((Math.random() - .5) * maxLeft)), maxLeft);
+        var y = Math.min(Math.max(0, ((Math.random() - .5) * maxTop)), maxTop);
+        this.healthRemaining = this.totalHealth;
+
+        this.div = this.createFlyDiv(x, y);
+    }
+
+    public move() {
+        var left = this.div.offsetLeft;
+        var width = this.div.offsetWidth;
+        var height = this.div.offsetHeight;
+        var top = this.div.offsetTop;
+
+        var maxLeft = Game.maxLeft - width;
+        var maxTop = Game.maxTop - height;
+
+
+        if (this.angle === undefined || Math.random() >= .90) {
+            this.angle = Math.random() * 2 * Math.PI;
+        }
+
+        var xChange = Math.cos(this.angle) * this.moveSpeed;
+        var yChange = Math.sin(this.angle) * this.moveSpeed;
+
+        // make sure we don't get stuck on walls...
+        if ((left + xChange) < 0 || (left + xChange) > maxLeft) {
+            xChange *= -1;
+            this.angle += Math.PI;
+        }
+        if ((top + yChange) < 0 || (top + yChange) > maxTop) {
+            yChange *= -1;
+            this.angle += Math.PI;
+        }
+
+        var newLeft = Math.min(Math.max(0, (left + xChange)), maxLeft);
+        var newTop = Math.min(Math.max(0, (top + yChange)), maxTop);
+
+        this.div.style.left = newLeft + "px";
+        this.div.style.top = newTop + "px";
+    }
+
+    // todo: move this part out to a helper class for creating divs
+    private createFlyDiv(x: number, y: number): HTMLDivElement {
+        var div = document.createElement("div");
+        div.style.top = y + "px";
+        div.style.left = x + "px";
+        div.style.width = this.width + "px";
+        div.style.height = this.height + "px";
+        div.classList.add("fly");
+        var that: Fly = this;
+        div.onclick = function () {
+            that.healthRemaining--;
+            Game.score();
+        };
+        document.body.appendChild(div);
+        return div;
+    }
+} 
